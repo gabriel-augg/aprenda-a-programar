aede01b35fe01a4174a8a4a84ef9b4f4b5d71efa
gabriel-augg <gabriel.auggg@gmail.com>
2022-08-25 18:59:40 -0300

    PraticaUm

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,11 +2,17 @@ let nome = "Gabriel Augusto"
 let email = "Gabriel.auggg@gmail.com"
 let profissao = "Suporte em TI"
 let idade = 17
-let viagem = "Canada"
+let viagem = "Canadá"
 console.log(nome)
 console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(viagem)
 
-console.log("Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o " + viagem + ". Confirma pra gente alguns dados?  A sua idade é " + idade + ",  você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+
+
+
+
+
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o ${viagem}. Confirma pra gente alguns dados? A sua idade é ${idade}, você trabalha com ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
