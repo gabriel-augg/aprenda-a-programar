a729d1b98cc25f5933548975709b45dc17f614e9
gabriel-augg <gabriel.auggg@gmail.com>
2022-08-24 23:08:46 -0300

    PraticaUmm

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,4 +3,8 @@ let email = "Gabriel.auggg@gmail.com"
 let profissao = "Suporte em TI"
 let idade = 17
 let viagem = "Canada"
-
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagem)
